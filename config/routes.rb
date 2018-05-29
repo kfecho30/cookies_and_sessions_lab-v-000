9cb98e050ae5a076270f50ee7b015a34e1b777bc
Kelsey Fecho <kfecho30@gmail.com>
2018-05-29 11:08:51 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get 'products' => 'products#index'
+  root 'products#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
